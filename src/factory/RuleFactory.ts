75a38520653565631f20b54bb3da86eb3f381514
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-05-15 10:45:15 +0200

    build ⚙️: register the new rules

--- src/factory/RuleFactory.ts
+++ src/factory/RuleFactory.ts
@@ -3,9 +3,13 @@ import { ArrayRule } from "../rules/ArrayRule";
 import { BetweenRule } from "../rules/BetweenRule";
 import { Boolean } from "../rules/Boolean";
 import { Email } from "../rules/Email";
+import FileType from "../rules/FileType";
+import { InArray } from "../rules/InArray";
 import { MaxRule } from "../rules/MaxRule";
 import { MinRule } from "../rules/MinRule";
+import { NotRegex } from "../rules/NotRegex";
 import { Numeric } from "../rules/Numeric";
+import { Regex } from "../rules/Regex";
 import { Required } from "../rules/Required";
 import { StringRule } from "../rules/StringRule";
 import { Url } from "../rules/Url";
@@ -41,6 +45,10 @@ export default class RuleFactory {
     RuleFactory.register("numeric", () => new Numeric());
     RuleFactory.register("string", () => new StringRule());
     RuleFactory.register("boolean", () => new Boolean());
+    RuleFactory.register("regex", () => new Regex());
+    RuleFactory.register("not-regex", () => new NotRegex());
+    RuleFactory.register("in-array", () => new InArray());
+    RuleFactory.register("file-type", () => new FileType());
   }
 }
 
